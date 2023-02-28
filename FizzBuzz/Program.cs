8a91a09ec8d1dd195589bce9e07706f07e358a27
Richard Morris <richmorris55@yahoo.com>
2023-02-28 12:39:57 -0800

    InitialCommit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static string FizzBuzz(int number)
+    {
+        if (number % 3 == 0)
+        {
+            return "Fizz";
+        } 
+        else if (number % 5 == 0)
+        {
+            return "Buzz";
+        }
+        
+        else if (number % 3 == 0 && number % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+        else
+        {
+            return number.ToString();
+        }
+    }
+}
+
