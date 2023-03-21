3e5de420fe0b149d724707fc2569e2c2ab6c9144
Richard Morris <richmorris55@yahoo.com>
2023-03-21 15:17:36 -0700

    InitialCommit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -3,23 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(FizzBuzz(25));
     }
 
     public static string FizzBuzz(int number)
     {
-        if (number % 3 == 0)
+        
+        if (number % 3 == 0 && number % 5 == 0)
+        {
+            return "FizzBuzz";
+        }
+
+        else if (number % 3 == 0)
         {
             return "Fizz";
         } 
         else if (number % 5 == 0)
         {
             return "Buzz";
-        }
-        
-        else if (number % 3 == 0 && number % 5 == 0)
-        {
-            return "FizzBuzz";
         }
         else
         {
